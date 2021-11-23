06a2b0e28cb06ab4d9240223ae511576a6fb374e
195024 <195024@AUMC-LAB2-33>
2021-11-23 11:39:10 +0500

    Add project files.

---
+++ 195026labmid/product.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace _195026labmid
+{
+    public class Product : INotifyPropertyChanged
+{
+    public int ProductID { get; set; }
+    public string ProductCode { get { return ProductID.ToString(); } }
+    public string ProductName { get; set; }
+    public string QuantityPerUnit { get; set; }
+    public decimal UnitPrice { get; set; }
+    public string UnitPriceString   
+    {
+        get
+        {
+            return UnitPrice.ToString("######.00");
+        }
+    }
+    public int UnitsInStock { get; set; }
+    public string UnitsInStockString
+    {
+        get
+        {
+            return
+UnitsInStock.ToString("#####0");
+        }
+    }
+    public int CategoryId { get; set; }
+    public event PropertyChangedEventHandler PropertyChanged;
+    private void NotifyPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
+}
+
+
+
